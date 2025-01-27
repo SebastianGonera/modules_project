57d1b170ff9dbc96fc1813ad6b4188d5c75af2f1
Sebastian <sebagonera@gmail.com>
2025-01-27 08:04:27 +0100

    Add download method to controller

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -6,6 +6,9 @@ use App\Http\Requests\ModuleRequest;
 use App\Models\Module;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use ZipArchive;
 
 class ModuleController extends Controller
 {
@@ -20,4 +23,75 @@ class ModuleController extends Controller
             ],
             201);
     }
+
+    public function download(string $id): BinaryFileResponse|JsonResponse
+    {
+        $module = Module::findOrFail($id);
+
+        $tempPath = storage_path('app/public/modules/' . $module->id);
+        $zipPath = storage_path('app/public/modules/' . $module->id . '.zip');
+
+        try {
+
+            File::makeDirectory($tempPath, 775, true, true);
+
+            File::put($tempPath . '/index.html', $this->getHTML());
+            File::put($tempPath . '/style.css', $this->getCSS($module));
+            File::put($tempPath . '/script.js', $this->getJS($module->link));
+
+            $zip = new ZipArchive();
+            if ($zip->open($zipPath, ZipArchive::CREATE) !== TRUE) {
+                $error = $zip->getStatusString();
+                throw new \Exception('Failed to create zip archive: ' . $error);
+            }
+
+            $zip->addFile($tempPath . '/index.html', 'index.html');
+            $zip->addFile($tempPath . '/style.css', 'style.css');
+            $zip->addFile($tempPath . '/script.js', 'script.js');
+            $zip->close();
+        } catch (\Exception $exception) {
+            if (File::exists($tempPath)) {
+                File::deleteDirectory($tempPath);
+            }
+            return response()->json(['message' => $exception->getMessage(), 'a'=>$exception->getTrace()], 500);
+        }
+
+        File::deleteDirectory($tempPath);
+
+        return response()->download($zipPath)->deleteFileAfterSend(true);
+    }
+
+    private function getHTML(): string
+    {
+        return "
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <link rel='stylesheet' href='style.css'>
+        <script src='script.js' defer></script>
+        <title>Models</title>
+    </head>
+    <body>
+        <div class='module'>Download module</div>
+    </body>
+</html>";
+    }
+
+    private function getCSS(Module $module): string
+    {
+        return ".module {
+        width: {$module->width};
+        height: {$module->height};
+        background-color: {$module->color};
+        cursor: pointer;
+        }";
+    }
+
+    private function getJS(string $link): string
+    {
+        return "document.querySelector('.module').addEventListener('click', function() {
+        window.location.href = '{$link}';
+        });";
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::controller(ModuleController::class)
     ->group(function () {
         Route::post('/modules', 'store');
+        Route::get('/modules/{id}/download', 'download');
     });
